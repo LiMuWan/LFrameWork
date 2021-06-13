7f36e9cda1aaddb84ea1f4160285db59200fd0bd
LiRui <18335929360@163.com>
2021-06-13 21:45:25 +0800

    BgMove

---
+++ Assets/Scripts/BgMove/sky.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class sky : MonoBehaviour {
+    RawImage img;
+    public float speed = 0.1f;
+	// Use this for initialization
+	void Start () {
+        this.img = this.GetComponent<RawImage>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Rect r = this.img.uvRect;
+        r.y += this.speed * Time.deltaTime;
+        this.img.uvRect = r;
+	}
+}
