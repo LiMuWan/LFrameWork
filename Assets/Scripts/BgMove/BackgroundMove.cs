381790711f4519ec2cfe559ca85b164b497f3ef0
LiRui <18335929360@163.com>
2021-06-13 21:55:47 +0800

    Frame Animation

--- Assets/Scripts/BgMove/BackgroundMove.cs
+++ Assets/Scripts/BgMove/BackgroundMove.cs
@@ -1,18 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
-public class sky : MonoBehaviour {
+public class BackgroundMove : MonoBehaviour
+{
     RawImage img;
     public float speed = 0.1f;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         this.img = this.GetComponent<RawImage>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         Rect r = this.img.uvRect;
         r.y += this.speed * Time.deltaTime;
         this.img.uvRect = r;
-	}
+    }
 }
